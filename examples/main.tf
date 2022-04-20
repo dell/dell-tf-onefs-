afd01c8ab4bb62cc6b5d5161a1361fbc035c3ae6
Ben Neigebauer <Ben_Neigebauer@dell.com>
2022-04-20 13:58:26 +0000

    test

--- examples/main.tf
+++ examples/main.tf
@@ -6,7 +6,6 @@ module "multi-node-example" {
   cluster_root_password = "Dell@1235"
 
   timezone = "Greenwich Mean Time"
-
   cluster_nodes = 5
 
   location = "centralus"
@@ -16,4 +15,5 @@ module "multi-node-example" {
   network_id = "/subscriptions/0629574c-1c80-4365-b0c6-3f5fdde6518e/resourceGroups/ctrl-thunderscale-central-app-rg/providers/Microsoft.Network/virtualNetworks/ctrl-azure_central-thunderscale-vnet"
   internal_prefix = "100.93.97.240/28"
   external_prefix = "100.93.104.240/28"
+
 }
