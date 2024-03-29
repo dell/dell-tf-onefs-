f246b25d0daa6986ac0ad6eafa68e81a146320dd
Tonworio Oguara <Tonworio.Oguara@emc.com>
2024-03-28 20:38:11 -0700

    prevent_deletion_if_contains_resources to true explicitely

--- main.tf
+++ main.tf
@@ -4,7 +4,12 @@
 
 provider "azurerm" {
   skip_provider_registration = true
-  features {}
+  #features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  }
 }
 
 locals {
