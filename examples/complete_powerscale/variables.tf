27282de4d6f5a6bc840cfd7b13537a97b2b8c2a4
Trent Ishii <trent.ishii@dell.com>
2024-05-20 09:16:01 -0700

    terraform fmt

--- examples/complete_powerscale/variables.tf
+++ examples/complete_powerscale/variables.tf
@@ -65,7 +65,7 @@ variable "external_gateway_address" {
 }
 
 variable "cluster_root_password" {
-  default  = null
+  default = null
 }
 
 variable "cluster_admin_username" {
@@ -73,7 +73,7 @@ variable "cluster_admin_username" {
 }
 
 variable "cluster_admin_password" {
-  default  = null
+  default = null
 }
 
 variable "credentials_hashed" {
